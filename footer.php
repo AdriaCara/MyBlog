a11cfe54354b21f2c5bec8868853d5f5de592c41
AdriaCara <adriaProgrammer@gmail.com>
2022-04-07 16:08:53 +0200

    New php pages.

--- footer.php
+++ footer.php
@@ -1,43 +1,5 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>My Blog</title>
-  </head>
-  <body>
-
-    <!-- Navbar -->
-    <nav class="navbar-expand-lg navbar navbar-dark bg-dark">
-        <div class="container-fluid">
-          <img src="./logos/Logo.png" href="./index.html" class="img-thumbnail" style="width: 15%;" alt="">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="./index.html">Inici</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./index.html">Features</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    
-    <div class="container">
-
-    </div>
-
-    <!-- Footer -->
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+<!-- Footer -->
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div class="col-md-4 d-flex align-items-center">
           <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
             <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
---
+++ headerPost.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>My Blog</title>
+  </head>
+  <body>
+
+    <!-- Navbar -->
+    <nav class="navbar-expand-lg navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <img src="./logos/Logo.png" href="./index.html" class="img-thumbnail" style="width: 15%;" alt="">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="./index.html">Inici</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./index.html">Features</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
