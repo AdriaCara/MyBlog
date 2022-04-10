010bdc71241dd75d6308618493c524a7972f75f5
AdriaCara <adriaProgrammer@gmail.com>
2022-04-11 01:34:09 +0200

    The main page has changed its format.

--- MyCodePassion.php
+++ MyCodePassion.php
@@ -11,6 +11,7 @@
         <p class="m-3">
             La programación actualmente se ha convertido en una profesion muy solicitada por todas las empresas, es raro ver a un negocio sin redes sociales y mucha más sin una pagina propia.
             <br />
+            <br />
             Muchas personas aprovechando la oportunidad han empezado a estudiar lenguajes de progrmación y trabajar de ello, para mi más que un trabajo es una pasion que durante todo el curso de DAM he ido dedicando tiempo extra a las actividades de clase para aprender más aún.
         </p>
     </div>
