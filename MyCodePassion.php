7438d0e55de3155734fdd60b9aa25a0644b113ec
AdriaCara <adriaProgrammer@gmail.com>
2022-04-13 21:48:52 +0200

    The post is more developed.

--- MyCodePassion.php
+++ MyCodePassion.php
@@ -15,9 +15,9 @@
             Muchas personas aprovechando la oportunidad han empezado a estudiar lenguajes de progrmación y trabajar de ello aprovechando esta demanda creciente en entorno de este sector. Eso a su vez genera gente que en vez de aprender a programar quieren programar para trabajar y eso vajo mi punto de vista es algo incorrecto; es algo que no afaborece del todo. 
         </p>
     </div>
-    <div class="bg-secondary d-flex text-white border border-3 border-dark" style="width: 75%; margin-left: 25%;">
+    <div class="bg-secondary d-flex text-white border border-3 border-dark" style="width: 75%; margin-left: 25%; margin-top: 1%">
         <p class="m-3">
-            En mi caso es distinto, no programo con un motivo principal y exclusivo de ganar dinero o un trabajo, mi motivo es completamente distinto, tiene que ver con una necesidad insasiable de aprender y seguir aprendiendo como si estubiera en la casilla de inicio.
+            En mi caso es distinto, no programo con un motivo principal y exclusivo de ganar dinero o un trabajo, mi motivo es ;completamente distinto, tiene que ver con una necesidad insasiable de aprender y seguir aprendiendo como si estubiera en la casilla de inicio.
             <br />
             <br />
             Desde que empece a programar hace algunos años me di cuenta de que es algo que me gusta, mi primer programa fue un juego de preguntas hehco con python y muchisimos if's y else's, el jugador tenia tres vidas y al ganar tenis el mitico mensaje de "A ganar a ganar, pollo para cenar".
@@ -29,9 +29,12 @@
             Ahora mismo he sido capaz de hacer este blog, mi portafolio, empezar a hacer una red social... muchos más proyectos de los que me siento orgulloso y sé que puedo mejorar muchisimo constantemente.
         </p>
     </div>
-    <div class="bg-secondary d-flex text-white border border-3 border-dark" style="width: 75%;">
+    <div class="bg-secondary d-flex text-white border border-3 border-dark" style="width: 75%; margin-top: 1%;">
         <p class="m-3">
-
+            Actualmente estoy estudiando DAM en la "<a class="text-white" href="https://mataro.escolapia.cat/">Escola Pia Santa Anna</a>" de Mataró, Cataluña. Me siento afortunado por estudiar con unos profesores que a simple vista se nota que son buenos profesionales del sector. Tanto el temario como las herramientas que usamos me parecen muy utiles y interesantes. Mi motivación para seguir aprendiendo es infinita, con muchas ganas de seguir sin parar para poder saciar dia a dia esas ganas.
+            <br />
+            <br />
+            Como reflexión final, si se tiene una afición nunca la dejes de lado, siempre he tenido la suerte de que aquello en lo que me gusto y me centre me fue bien(haciendo trucos de magia, jugando a futbol, practicando ajedrez, etc) pero fue suerte..., por eso siempre haz aquello que te guste porque dejar tu felicidad a meerced de la suerte no te hara feliz encambio hacer lo que te gusta si y si tienes suerte en ello, disfruta tanto como puedas.
         </p>
     </div>
 </div>
