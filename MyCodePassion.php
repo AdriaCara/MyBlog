ddf1e6859f2b4434c34b9386210f2b06ef280a1b
AdriaCara <adriaProgrammer@gmail.com>
2022-04-13 21:28:26 +0200

    Continuing my post and changing the main page of my blog.

--- MyCodePassion.php
+++ MyCodePassion.php
@@ -7,12 +7,31 @@
     <br />
     <h1 class="text-center">Mi pasion por la programación.</h1>
     <br />
-    <div class="bg-secondary d-flex text-white" style="width: 75%">
+    <div class="bg-secondary d-flex text-white border border-3 border-dark" style="width: 75%;">
         <p class="m-3">
             La programación actualmente se ha convertido en una profesion muy solicitada por todas las empresas, es raro ver a un negocio sin redes sociales y mucha más sin una pagina propia.
             <br />
             <br />
-            Muchas personas aprovechando la oportunidad han empezado a estudiar lenguajes de progrmación y trabajar de ello, para mi más que un trabajo es una pasion que durante todo el curso de DAM he ido dedicando tiempo extra a las actividades de clase para aprender más aún.
+            Muchas personas aprovechando la oportunidad han empezado a estudiar lenguajes de progrmación y trabajar de ello aprovechando esta demanda creciente en entorno de este sector. Eso a su vez genera gente que en vez de aprender a programar quieren programar para trabajar y eso vajo mi punto de vista es algo incorrecto; es algo que no afaborece del todo. 
+        </p>
+    </div>
+    <div class="bg-secondary d-flex text-white border border-3 border-dark" style="width: 75%; margin-left: 25%;">
+        <p class="m-3">
+            En mi caso es distinto, no programo con un motivo principal y exclusivo de ganar dinero o un trabajo, mi motivo es completamente distinto, tiene que ver con una necesidad insasiable de aprender y seguir aprendiendo como si estubiera en la casilla de inicio.
+            <br />
+            <br />
+            Desde que empece a programar hace algunos años me di cuenta de que es algo que me gusta, mi primer programa fue un juego de preguntas hehco con python y muchisimos if's y else's, el jugador tenia tres vidas y al ganar tenis el mitico mensaje de "A ganar a ganar, pollo para cenar".
+            <br/>
+            <br />
+            Mi siguiente proyecto fue hacer una pagina web para el cumple de un muy buen amigo mio, donde habian fotos juntos, un tetxo, colores, intente poner un audio con una cancion con la que nos reiamos mucho, todo el HTML, CSS que utilize lo aprendi en la aplicación de <a class="text-white" href="https://www.sololearn.com/">"Solo learn"</a> y la verdad fue algo muy pero que muy basico.
+            <br />
+            <br />
+            Ahora mismo he sido capaz de hacer este blog, mi portafolio, empezar a hacer una red social... muchos más proyectos de los que me siento orgulloso y sé que puedo mejorar muchisimo constantemente.
+        </p>
+    </div>
+    <div class="bg-secondary d-flex text-white border border-3 border-dark" style="width: 75%;">
+        <p class="m-3">
+
         </p>
     </div>
 </div>
