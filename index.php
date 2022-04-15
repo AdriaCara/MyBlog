f3c01cfd5f049afc80439c3fa36f1bb900197423
AdriaCara <adriaProgrammer@gmail.com>
2022-04-15 22:53:48 +0200

    Changed the HTML document for a PHP.

---
+++ index.php
@@ -0,0 +1,149 @@
+<?php
+    require 'headerPost.php';
+?>
+
+<main class="container text-white">
+    <div class="p-4 p-md-5 mb-4 text-white rounded" style="background-color: #1F1237;>
+        <div class=" col-md-6 px-0">
+        <h1 class="display-4 fst-italic">Mi pasion por la programación.</h1>
+        <p class="lead my-3">La programación actualmente se ha convertido en
+            una profesion muy solicitada por todas las empresas, es raro ver a
+            un negocio sin redes sociales y mucha más sin una pagina propia.</p>
+        <p class="lead mb-0"><a href="./MyCodePassion.php" class="text-white
+              fw-bold">Seguir leyendo...</a></p>
+    </div>
+    </div>
+
+    <div class="row mb-2">
+        <div class="col-md-6">
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4
+            shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                    <strong class="d-inline-block mb-2 text-primary">Sobre mí</strong>
+                    <h3 class="mb-0">Mi portafolio</h3>
+                    <div class="mb-1 text-muted">Abril 11</div>
+                    <p class="card-text mb-auto">Aquí podras ver mi portafolio.</p>
+                    <a href="https://adriacara.github.io/portfolio/" class="stretched-link">Ir a la pagina</a>
+                </div>
+                <div class="col-auto d-none d-lg-block">
+                    <img src="./MyPhotos/MyPhoto_1.jpg" class="bd-placeholder-img" width="200" height="250">
+                    <title>Una foto mia</title>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4
+            shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                    <strong class="d-inline-block mb-2 text-success">Proyectos</strong>
+                    <h3 class="mb-0">Mis proyectos</h3>
+                    <div class="mb-1 text-muted">Abril 11</div>
+                    <p class="mb-auto">Aquí puedes ver mis proyectos.</p>
+                    <a href="https://adriacara.github.io/portfolio/MyProjects.html" class="stretched-link">Ir a la
+                        pagina</a>
+                </div>
+                <div class="col-auto d-none d-lg-block">
+                    <img src="./PhotosBlog/MyProjects.PNG" class="bd-placeholder-img" width="200" height="250">
+                    <title>Una foto mia</title>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row g-5">
+        <div class="col-md-8">
+            <h3 class="pb-4 mb-4 fst-italic border-bottom">
+                Acerca de este blog
+            </h3>
+
+            <article class="blog-post">
+                <h2 class="blog-post-title">Que no es</h2>
+                <p class="blog-post-meta">Abril 11, 2022 por <a href="https://adriacara.github.io/portfolio/">Adrià</a>
+                </p>
+                <p>Para definir algo a veces es mejor empezar por lo que no es, este
+                    blog no tiene la intención de ser un sitio para la divulgación de
+                    la verdad, más bien un espacio de reflexión para aquellos
+                    inquietos que les gusta cuestionarse y aprender mediante el
+                    pensamiento, si quieres aprender hechos mejor ves a sitios para
+                    leer artículos científicos.</p>
+                <hr>
+                <p>Teniendo en cuenta que no es este blog podemos definir que es:</p>
+                <h2>Que es</h2>
+                <blockquote class="blockquote">
+                    <p>Definición de que es mi blog.</p>
+                </blockquote>
+                <p>Mi blog se como el gimnasio para la mente, un sitio donde planteo
+                    preguntas o trato temas actuales para generar una opinion propia y
+                    hacer reflexionar a los usuarios. En este blog cada post estará
+                    influenciado por mis principios que como todo en esta vida, no son
+                    inamovibles, que las cosas sean de una manera, no significa que
+                    deban ser así para siempre.</p>
+                <h3>Mis principios</h3>
+                <p>En este mundo considero que hay unos principios que no se
+                    deberían corromper en ningún momento en una persona decente, estos
+                    son:</p>
+                <ul>
+                    <li>Mejora constantemente</li>
+                    <li>No mentir</li>
+                    <li>Amarse a uno mismo sin ser tóxico</li>
+                    <li>Ayudar siempre que puedas</li>
+                    <li>Saber que todo tiene algo positivo</li>
+                    <li>No malgastes</li>
+                </ul>
+            </article>
+            <hr>
+            <a class="text-white" href="http://myblog.test/MyCodePassion.php" style="text-decoration: none;">
+                <article class="blog-post">
+                    <h2 class="blog-post-title">Mi pasion por la programación</h2>
+                    <p class="blog-post-meta">La programación actualmente se ha convertido en una profesion muy
+                        solicitada por todas las empresas, es raro ver a un negocio sin redes sociales y mucha más sin
+                        una pagina propia.</p>
+                </article>
+            </a>
+
+        </div>
+
+        <div class="col-md-4">
+            <div class="position-sticky" style="top: 2rem;">
+                <div class="p-4 mb-3 rounded" style="background-color: #4B2D84;">
+                    <h4 class="fst-italic">Mi blog</h4>
+                    <p class="mb-0">Esta página web es un blog personal el cual tiene
+                        como intención que las personas que se pasen por aquí se
+                        entretengan leyendo artículos interesantes que he escrito y
+                        diseñado yo mismo.</p>
+                </div>
+
+                <div class="p-4">
+                    <h4 class="fst-italic">Archives</h4>
+                    <ol class="list-unstyled mb-0">
+                        <li><a href="./MyCodePassion.php">Mi pasion por la programación</a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                    </ol>
+                </div>
+
+                <div class="p-4">
+                    <h4 class="fst-italic">Me puedes encontrar</h4>
+                    <ol class="list-unstyled">
+                        <li><a href="https://github.com/AdriaCara">GitHub</a></li>
+                        <li><a href="https://mastodon.social/web/@adriaProgrammer">Mastodon</a></li>
+                        <li><a href="https://www.linkedin.com/in/adriacara/">Linkedin</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+<!-- Footer -->
+<?php
+  require 'footer.php';
+?>
