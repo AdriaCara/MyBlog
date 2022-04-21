076f52b656d47a08e7734e12a2d520a24fe11a10
AdriaCara <adriaProgrammer@gmail.com>
2022-04-22 00:18:18 +0200

    Finished the second post.

--- charity.php
+++ charity.php
@@ -9,24 +9,27 @@
     <br />
     <div class="d-flex text-white border border-5 border-dark" style="width: 75%; background-color: #29164B;">
         <p class="m-3">
-            La caridad se define según la RAE como una "actitud solidiaria con el sufrimiento ajeno.", muchas veces vemos a alguien pidiendo ya sea en la calle, transporte publico o incluso las redes repetidas veces.
+        La caridad se define, según la RAE, como una "actitud solidaria con el sufrimiento ajeno.", muchas veces vemos a alguien pidiendo, ya sea en la calle, transporte público o incluso las redes repetidas veces.
             <br />
             En esos casos hay gente que o bien da dinero a esa persona o simplemente sigue con su vida sin involucrarse con la suya.
         </p>
     </div>
     <div class="d-flex text-white border border-5 border-dark" style="width: 75%; background-color: #29164B; margin-left: 25%; margin-top: 1%">
         <p class="m-3">
-            En mi caso muchas veces doy algo de dinero porque no me cuesta nada, incluso a veces casi me se aprovechan de mi por ello.
+        En mi caso muchas veces doy algo de dinero porque no me cuesta nada, incluso a veces casi se me aprovechan de mí por ello.
             <br />
-            A veces me pregunto si mi acto de <b>caridad</b> es altruismo o por comprarme un momento de bienestar conmigo mismo?, lo hago por lastima o por si algun dia soy yo el que pide?
+            ¿A veces me pregunto si mi acto de <b>caridad</b> es altruismo o por comprarme un momento de bienestar conmigo mismo?, lo hago por lástima o por si algún día soy yo el que pide?
             <br />
             <br />
-            En mi caso no creo en el karma ni en poderes sobrecientificos que sobrepasen logica alguna
+            En mi caso no creo en el karma ni en poderes sobrenaturales que sobrepasen lógica alguna. En mi opinión, no hay una forma correcta de actuar delante alguien que pide, obviamente sin utilizar la violencia ni la mala educación.
+            <br />
+            <br />
+            Pensando sobre este tema me he dado cuenta de que ayudar a los demás se hace para obtener una recompensa, ya sea un poco de químicos para nuestro cerebro que nos proporcionen felicidad, sea por el motivo que sea al ayudar. Esto hace que ayudar no sea un acto desinteresado en la mayoría de sus casos.
         </p>
     </div>
     <div class="d-flex text-white border border-5 border-dark" style="width: 75%; background-color: #29164B; margin-top: 1%;">
         <p class="m-3">
-            
+        En conclusión, ayudar solo se debería considerar de forma desinteresada cuando al hacerlo realmente no se consiguiera nada, a cambio, al tener que prescindir de algo muy importante y que no lo harías por ayudar.
         </p>
     </div>
     <br />
