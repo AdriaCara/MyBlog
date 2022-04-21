0757ac0c7b97710d14e9c5110f2400df28ffe31d
AdriaCara <adriaProgrammer@gmail.com>
2022-04-21 02:47:00 +0200

    New post and changed the footer.

---
+++ charity.php
@@ -0,0 +1,34 @@
+<?php
+    require 'headerPost.php';
+?>
+
+<div class="container" style="background-color: #1F1237;">
+    <br />
+    <br />
+    <h1 class="text-center text-decoration-underline text-white">Caridad.</h1>
+    <br />
+    <div class="d-flex text-white border border-5 border-dark" style="width: 75%; background-color: #29164B;">
+        <p class="m-3">
+            La caridad se define según la RAE como una "actitud solidiaria con el sufrimiento ajeno.", muchas veces vemos a alguien pidiendo ya sea en la calle, transporte publico o incluso las redes repetidas veces.
+            <br />
+            En esos casos hay gente que o bien da dinero a esa persona o simplemente sigue con su vida sin involucrarse con la suya.
+        </p>
+    </div>
+    <div class="d-flex text-white border border-5 border-dark" style="width: 75%; background-color: #29164B; margin-left: 25%; margin-top: 1%">
+        <p class="m-3">
+            En mi caso muchas veces doy algo de dinero porque no me cuesta nada, incluso a veces casi me se aprovechan de mi por ello.
+            <br />
+            A veces me pregunto si mi acto de <b>caridad</b> es altruismo o por comprarme un momento de bienestar conmigo mismo?
+        </p>
+    </div>
+    <div class="d-flex text-white border border-5 border-dark" style="width: 75%; background-color: #29164B; margin-top: 1%;">
+        <p class="m-3">
+            
+        </p>
+    </div>
+    <br />
+</div>
+
+<?php
+    require 'footer.php';
+?>
